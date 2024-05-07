b459c533569a9223ad52d408b791b6071d0b99f5
ravi italiya <italiyaravi704@gmail.com>
2024-05-07 16:08:52 +0530

    [add]-password hash throw bcrypt

--- backend/controller/registerUser.ts
+++ backend/controller/registerUser.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import UserModel from "../Models/UserModel";
+import bcrypt from "bcrypt"
+
 
 const register = async (req: Request, res: Response) => {
   try {
@@ -7,10 +9,16 @@ const register = async (req: Request, res: Response) => {
 
     const checkEmail = await UserModel.findOne({ email });
 
+    //email verification
     if(checkEmail){
         return res.status(400).json({msg: "Email already exists."})
     }
+
+    //password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     
+
 
   } catch (error) {}
 };
