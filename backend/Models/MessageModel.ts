d0b3367199d6f2a4004119f2f99f82ce3af14339
ravi italiya <italiyaravi704@gmail.com>
2024-05-07 15:49:32 +0530

    [add]-Message Model

---
+++ backend/Models/MessageModel.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const MessageModel = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      default: "",
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    videoUrl: {
+      type: String,
+      default: "",
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("MessageModel", MessageModel);
