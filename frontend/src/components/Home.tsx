d2eb739b2ce8be4e1a39d6874735065ba617cb47
ravi italiya <italiyaravi704@gmail.com>
2024-05-12 11:51:07 +0530

    [add]- user detail api in home page

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -4,6 +4,30 @@ import axios from "axios";
 const Home = () => {
 
 
+  const getUserdata = async ()=>{
+
+
+  const URL = `${import.meta.env.VITE_BACKEND_URL}/api/user-details`;
+
+    try {
+      const response = await axios({
+        method: "get",
+        url: URL,
+        withCredentials: true,
+      })
+
+      const userData = response.data;
+
+      console.log(userData);
+      
+    }
+      catch (err) {
+        console.log(err);
+        
+      }
+  }
+
+  getUserdata()
       
   return (
     <div>
