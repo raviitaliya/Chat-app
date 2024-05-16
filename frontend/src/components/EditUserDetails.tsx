c23142439a159ff4f4c8bc55e8454a3059313c1f
ravi italiya <italiyaravi704@gmail.com>
2024-05-16 22:09:19 +0530

    wefw

--- frontend/src/components/EditUserDetails.tsx
+++ frontend/src/components/EditUserDetails.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const EditUserDetails = ({ data }) => {
   return (
     <div className="fixed  left-0 right-0 top-0 bottom-0 bg-slate-700 bg-opacity-40 flex items-center justify-center">
-      <div className="w-[500px] h-[300px] bg-white rounded-md">EditUserDetails</div>
+      <div className="max-w-sm p-4 rounded-md bg-white">EditUserDetails</div>
     </div>
   );
 };
