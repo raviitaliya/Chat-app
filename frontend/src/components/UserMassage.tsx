1bf833ec87864d648e876f14e1e05a8c3560e753
ravi italiya <italiyaravi704@gmail.com>
2024-05-23 23:20:43 +0530

    [add]-msg input bar

--- frontend/src/components/UserMassage.tsx
+++ frontend/src/components/UserMassage.tsx
@@ -2,6 +2,10 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { GlobalStore } from "../Redux/Store";
 import { useEffect, useState } from "react";
+import { VscSmiley } from "react-icons/vsc";
+import { FiPaperclip } from "react-icons/fi";
+import { GoPaperAirplane } from "react-icons/go";
+import EmojiPicker from 'emoji-picker-react';
 
 const UserMassage = () => {
   const socket = useSelector<GlobalStore, Socket>(
@@ -28,6 +32,10 @@ const UserMassage = () => {
 
   console.log("statedata", userData);
 
+  const handleEmoji = () => {
+    return 
+  };
+
   return (
     <div>
       <div className="w-full h-20 px-4 border-b-2 border-slate-300 shadow-sm flex gap-5 items-center">
@@ -36,7 +44,7 @@ const UserMassage = () => {
           className="w-12 h-12 rounded-full object-cover"
         />
         <div>
-          <h1 className="font-medium text-xl">{userData.name}</h1>
+          <h1 className="font-medium text-lg">{userData.name}</h1>
           <h1
             className={`text-sm ${
               userData.online ? "text-primary" : "text-slate-400"
@@ -45,6 +53,21 @@ const UserMassage = () => {
             {userData.online ? "online" : "offline"}
           </h1>
         </div>
+      </div>
+      <section className="h-[calc(100vh-140px)] p-4">
+        <h1>show massge</h1>
+      </section>
+      <div className="flex items-center w-full border-t-2 h-[60px] p-1">
+        <button onClick={handleEmoji}>
+          <VscSmiley  className="text-2xl ml-2 h-full" />
+        </button>
+        <FiPaperclip className="text-2xl h-full ml-5" />
+        <input
+          type="text"
+          className="w-full h-full text-lg p-3 mb-1 ml-5 rounded-md focus:outline-none"
+          placeholder="Type a message"
+        />
+        <GoPaperAirplane className="text-2xl mr-3 h-full" />
       </div>
     </div>
   );
